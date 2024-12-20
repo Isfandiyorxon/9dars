3f9d9d28705eed71ea450809acc64a95ebacdc92
Isfandiyor <foziljonovisfandiyor@gmail.com>
2024-12-20 22:00:19 +0500

    yukladi 11 dars 1-qism

--- gullar/gul/urls.py
+++ gullar/gul/urls.py
@@ -1,11 +1,15 @@
 
-from .views import home,gul_by_tur,gul_detail,add_gul
+from .views import home,gul_by_tur,gul_detail,add_gul,gul_update,delete_gul
 from django.urls import path
 
 
 
-urlpatterns = [path('',home,name='home'),
+urlpatterns = [
+    path('',home,name='home'),
                path('turlar/<int:tur_id>/', gul_by_tur, name='gul_by_tur'),
                path('gullar/<int:pk>/', gul_detail, name='detail'),
-                   path('add-gul/',add_gul,name='add_gul'),
+               path('add-gul/',add_gul,name='add_gul'),
+               path('update-gul/<int:pk>/',gul_update,name='update-gul'),
+               path('delete-gul/<int:pk>/', delete_gul, name='delete-gul'),
+
                ]
--- gullar/gul/views.py
+++ gullar/gul/views.py
@@ -49,4 +49,42 @@ def add_gul(request):
         'form':form
     }
     return render(request,'add_gul.html',context)
+def gul_update(request,pk):
+    gul=get_object_or_404(Gullar,pk=pk)
+    if request.method=='POST':
+        form=GulForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            gul.name=form.cleaned_data.get("name")
+            gul.color=form.cleaned_data.get("color")
+            gul.tur=form.cleaned_data.get("tur")
+            gul.photo=form.cleaned_data.get("photo") if form.cleaned_data.get("photo") else gul.photo
+            gul.about=form.cleaned_data.get("about")
+            gul.save()
+            return redirect('detail',pk=gul.pk)
 
+
+    form=GulForm(initial={
+         "name": gul.name,
+        "about":gul.about,
+        "photo":gul.photo,
+        "tur":gul.tur,
+        "color":gul.color
+    })
+    context={
+        "form":form,
+        "gul":gul
+        }
+    return render(request,'add_gul.html',context)
+
+
+
+def delete_gul(request,pk):
+    gul=get_object_or_404(Gullar, pk=pk)
+    print("salom")
+    if request.method=='POST':
+        gul.delete()
+        return redirect('home')
+    context={
+        'gul':gul
+    }
+    return render(request,'gul_delete.html',context)
